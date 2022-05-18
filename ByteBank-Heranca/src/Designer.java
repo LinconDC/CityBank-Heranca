372f72a07346a5d072eea51fe635ad1ef58623a3
Lincon Martins <linconmartins18@gmail.com>
2022-05-18 16:41:16 -0300

    generic types

---
+++ ByteBank-Heranca/src/Designer.java
@@ -0,0 +1,8 @@
+
+public class Designer extends Funcionario {
+
+	public double getBonus() {
+		System.out.println("chamando o metodo de bonificacao do design");
+		return + 200;
+	}
+}
--- ByteBank-Heranca/src/TesteReferencias.java
+++ ByteBank-Heranca/src/TesteReferencias.java
@@ -12,11 +12,15 @@ public class TesteReferencias {
 
 		EditorDeVideo ev = new EditorDeVideo();
 		f.setSalary(2500.0);
+		
+		Designer ds = new Designer();
+		ds.setSalary(2000.0);
 
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(g1);
 		controle.registra(f);
 		controle.registra(ev);
+		controle.registra(ds);
 
 		System.out.println(controle.getSoma());
 	}
