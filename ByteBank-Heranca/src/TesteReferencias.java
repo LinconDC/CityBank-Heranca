ddea1949f03f8f9f164cb8dabb762e104b0544b5
Lincon Martins <linconmartins18@gmail.com>
2022-05-18 20:17:50 -0300

    account abstract

--- ByteBank-Heranca/src/TesteReferencias.java
+++ ByteBank-Heranca/src/TesteReferencias.java
@@ -7,18 +7,14 @@ public class TesteReferencias {
 		g1.setNome("Marcos");
 		g1.setSalary(5000.0);
 
-		Funcionario f = new Funcionario();
-		f.setSalary(2000.0);
-
 		EditorDeVideo ev = new EditorDeVideo();
-		f.setSalary(2500.0);
-		
+		ev.setSalary(2500.0);
+
 		Designer ds = new Designer();
 		ds.setSalary(2000.0);
 
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(g1);
-		controle.registra(f);
 		controle.registra(ev);
 		controle.registra(ds);
 
