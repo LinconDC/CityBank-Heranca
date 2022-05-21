4e270901f3fac33021461c2d94718806f9148620
LinconDC <linconmartins18@gmail.com>
2022-05-21 16:01:01 -0300

    Added interfaces and abstract methods.

--- ByteBank-Heranca/src/Gerente.java
+++ ByteBank-Heranca/src/Gerente.java
@@ -1,8 +1,24 @@
 
-public class Gerente extends Funcionario{
+public class Gerente extends Funcionario implements Autenticavel {
+	
+	private int senha;
 
 	public double getBonus() {
 		System.out.println("Chamando o metodo de bonificacao do gerente");
 		return super.getSalary();
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
